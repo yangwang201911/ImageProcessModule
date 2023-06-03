0ec7b3681fc0d2a6d1599c128bb004f22cef826a
Yang Wang <wangyang.xjtu@foxmail.com>
2023-06-03 15:28:11 +0800

    1. add test case to check if base64 encoder/decoder works on image data. 2. Implement the basic funtion of template matcher.
    Signed-off-by: Yang Wang <wangyang.xjtu@foxmail.com>

--- imageProcessAlgor/cutImageAlgr.cpp
+++ imageProcessAlgor/cutImageAlgr.cpp
@@ -10,19 +10,18 @@ using namespace cv;
 using namespace std;
 extern "C"
 {
-	// 声明回调函数类型
 	typedef void (*DebugCallback)(const char *);
 
-	// 定义全局变量保存回调函数
+	// Define the callback function pointer
 	static DebugCallback s_DebugCallback = nullptr;
 
-	// 设置回调函数
+	// Set callback functon
 	__declspec(dllexport) void SetDebugCallback(DebugCallback callback)
 	{
 		s_DebugCallback = callback;
 	}
 
-	// 调试输出函数
+	// Print the debug infomation like the argument of function calling and returned value.
 	__declspec(dllexport) void DebugPrint(const char *message)
 	{
 		if (s_DebugCallback)
@@ -31,15 +30,35 @@ extern "C"
 		}
 	}
 
-	// 基本测试函数
+	// Basic function test. Just check if the debug informate output works well.
 	__declspec(dllexport) void BaseFunctionTest(char *data, int length)
 	{
 		char msg[2048] = "";
 		sprintf_s(msg, sizeof(msg) - strlen(msg), "Function Name:\n\tBaseFunctionTest\nBrief:\n\tPrint the debug msg to check if Debug callback works\n"
-					   "Parameters:\n\tdata, char*, Pointer. point to the data buffer;\n\tlength, int, length of the data;\n"
-					   "Return:\n\tvoid\n Argument:\n\tdata = %s\n\tlength = %d\n",
+												  "Parameters:\n\tdata, char*, Pointer. point to the data buffer;\n\tlength, int, length of the data;\n"
+												  "Return:\n\tvoid\n Argument:\n\tdata = %s\n\tlength = %d\n",
 				  data, length);
 		DebugPrint(msg);
+	}
+
+	// Template matcher
+	__declspec(dllexport) int MatchTarget(char *image, int image_size, char *target, int target_size, int &loc_x, int &loc_y)
+	{
+		int quality = -1;
+		char msg[256] = "";
+		sprintf_s(msg, sizeof(msg) - strlen(msg), "Image length: %d\nTarget length: %d\n", image_size, target_size);
+		loc_x = -1;
+		loc_y = -1;
+		std::string imageData = Base64Decoder(image, image_size);
+		vector<uchar> decodedImage(imageData.begin(), imageData.end());
+		cv::Mat decodedMat = imdecode(decodedImage, cv::IMREAD_COLOR);
+		loc_x = decodedMat.rows / 2;
+		loc_y = decodedMat.cols / 2;
+		sprintf_s(msg, sizeof(msg) - strlen(msg), "Image size: %d x %d\nTarget loc: (%d,%d)\n", decodedMat.rows, decodedMat.cols, loc_x, loc_y);
+		DebugPrint(msg);
+		//cv::imshow("image", decodedMat);
+		//cv::waitKey(0);
+		return quality;
 	}
 }
 
@@ -47,8 +66,8 @@ __declspec(dllexport) std::string Base64Encoder(char *data, int length)
 {
 	char msg[2048] = "";
 	sprintf_s(msg, sizeof(msg) - strlen(msg), "Function Name:\n\tBase64Encoder\nBrief:\n\tEncode the input string with Base64.\n"
-				   "Parameters:\n\tdata, char*, Pointer. point to the source data;\n\tlength, int, length of the data;\n"
-				   "Return:\n\tvoid\n Argument:\n\tdata = %s\n\tlength = %d\n",
+											  "Parameters:\n\tdata, char*, Pointer. point to the source data;\n\tlength, int, length of the data;\n"
+											  "Return:\n\tvoid\n Argument:\n\tdata = %p\n\tlength = %d\n",
 			  data, length);
 	std::string source(data, length);
 	DWORD binarySize = static_cast<DWORD>(length);
@@ -73,7 +92,7 @@ __declspec(dllexport) std::string Base64Encoder(char *data, int length)
 	if (s_DebugCallback)
 		DebugPrint(msg);
 	else
-		std::cout << std::string(msg) << std::endl; 
+		std::cout << std::string(msg) << std::endl;
 	return encodedData;
 }
 
@@ -81,10 +100,10 @@ __declspec(dllexport) std::string Base64Decoder(char *data, int length)
 {
 	char msg[2048] = "";
 	sprintf_s(msg, sizeof(msg) - strlen(msg), "Function Name:\n\tBase64Decoder\nBrief:\n\tDecode the input string with Base64.\n"
-				   "Parameters:\n\tdata, char*, Pointer. point to the encoded data;\n\tlength, int, length of the data;\n"
-				   "Return:\n\tvoid\n Argument:\n\tdata = %s\n\tlength = %d\n",
+											  "Parameters:\n\tdata, char*, Pointer. point to the encoded data;\n\tlength, int, length of the data;\n"
+											  "Return:\n\tvoid\n Argument:\n\tdata = %p\n\tlength = %d\n",
 			  data, length);
-	// Base64 编码后的字符串
+	// Base64 encoded data 
 	std::string source(data, length);
 	DWORD dwFlags = CRYPT_STRING_BASE64;
 	DWORD binarySize = 0;
@@ -105,6 +124,6 @@ __declspec(dllexport) std::string Base64Decoder(char *data, int length)
 	if (s_DebugCallback)
 		DebugPrint(msg);
 	else
-		std::cout << std::string(msg) << std::endl; 
+		std::cout << std::string(msg) << std::endl;
 	return binaryData;
 }
--- imageProcessAlgor/cutImageAlgr.h
+++ imageProcessAlgor/cutImageAlgr.h
@@ -4,9 +4,11 @@
 #pragma once
 
 #include <iostream>
-extern "C" {
-	__declspec(dllexport) void BaseFunctionTest(char* data, int length);
+extern "C"
+{
+	__declspec(dllexport) void BaseFunctionTest(char *data, int length);
 	__declspec(dllexport) void DebugPrint(const char *message);
+	__declspec(dllexport) int MatchTarget(char *image, int image_size, char *target, int target_size, int &loc_x, int &loc_y);
 }
 __declspec(dllexport) std::string Base64Encoder(char *data, int length);
 __declspec(dllexport) std::string Base64Decoder(char *data, int length);
